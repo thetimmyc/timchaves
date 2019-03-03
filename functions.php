bffcbbe252dfe8ff90435ab348d6726fff6c5aa5
timchaves <tim.chaves@gmail.com>
2019-03-03 12:34:21 -0700

    a whole buncha styling

--- functions.php
+++ functions.php
@@ -41,3 +41,38 @@ add_action('wp_enqueue_scripts', 'my_ts_enqueue_child', 11);
 
 // Disable parent CSS enqueue
 add_filter('bunyad_enqueue_core_css', '__return_false');
+
+/* Book shortcode widget */
+function books_widget_shortcode($atts = [], $content = null, $tag = '')
+{
+    global $post;
+    $books = json_decode(get_post_meta( $post->ID, 'books', true ));
+
+    if ($books) {
+        $o = '';
+        $o .= '<li class="widget books-widget">';
+        $o .= '  <h5 class="widget-title">Books that influenced this article</h5>';
+        $o .= '  <div class="ts-row ts-row-small-gutters">';
+
+        foreach ($books as $book) {
+            $o .= '<div class="col-4">';
+            $o .= '  <a href="' . $book->link . '">';
+            $o .= '    <img src="' . $book->image_url . '" />';
+            $o .= '  </a>';
+            $o .= '</div>';
+        }
+        $o .= '  </div>';
+        $o .= '</li>';
+    }
+
+    return $o;
+
+}
+
+
+function shortcodes_init()
+{
+    add_shortcode('books', 'books_widget_shortcode');
+}
+
+add_action('init', 'shortcodes_init');
